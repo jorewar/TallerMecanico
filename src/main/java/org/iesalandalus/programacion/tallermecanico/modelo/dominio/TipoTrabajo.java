749db8de5efab6172cd0b4911aac355722565bf8
Jorge <inelcom1540@gmail.com>
2024-04-18 12:43:53 +0200

    - [Sprint inicial](../../tree/sprint_inicial)

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/TipoTrabajo.java
@@ -0,0 +1,15 @@
+﻿package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
+
+public enum TipoTrabajo {
+    MECANICO, REVISION;
+
+    private String nombre;
+    private String TipoTrabajo;
+
+    public static Trabajo Get(Trabajo trabajo) {
+        return null;
+    }
+
+
+
+}
